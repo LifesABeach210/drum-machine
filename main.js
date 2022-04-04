ad4ef9863813b0fcae5060180352e50dfcf2c94d
Dan <danielrothwell622@gmail.com>
2022-04-04 00:13:30 -0400

    hello

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-let count = 0;
+
+
 // Setup 'tick' sound
 const tick = new Audio('sounds/tick.mp3');
 const hat = new Audio('sounds/hi-hat.mp3');
@@ -10,6 +11,10 @@ let metronome = document.querySelector("#metronome");
 let kickDrum = document.querySelector("#kick-drum");
 let snareDrum = document.querySelector("#snare-drum");
 let hiHat = document.querySelector("#hi-hat");
+let kickDrumTiming = document.querySelector('#kick-drum-timing');
+let snareDrumTiming = document.querySelector("#snare-drum-timing");
+let metronomeTiming = document.querySelector("#metronome-timing");
+let hiHatTiming = document.querySelector("#hi-hat-timing");
 
 let soundBeat = 0;
 let setBeat = document.querySelector(".setBeat");
@@ -19,19 +24,42 @@ let setBeat = document.querySelector(".setBeat");
 function update() {
 
     // Play the 'tick' sound
-    tick.play();
+tick.play();
 soundBeat++;
 if (soundBeat % 4 === 0) {
     tock.play();
 }
 
-setBeat.innerText = (soundBeat % 4 === 0)+1;
+setBeat.innerText = (soundBeat % 4)+1;
 
+if (metronome.checked===true) {
+   if (Number(setBeat.innerText)===Number(metronomeTiming.value)) {
+    tick.play();
+   }
+   
+   
+}
 
+if (hiHat.checked===true) {
+    if (Number(setBeat.innerText)===Number(hiHatTiming.value)) {
+        hat.play();
+    }
+    
+}
 
+if (snareDrum.checked===true) {
+   if (Number(setBeat.innerText)===Number(snareDrumTiming.value)) {
+    snare.play();
+   }
+    
+}
 
-
-
+if (kickDrum.checked===true) {
+   if (Number(setBeat.innerText)===Number(kickDrumTiming.value)) {
+    kick.play();
+   }
+   
+}
 
 
 }
